8ffa896014cd388e8213d5ba128ff7461053b2f1
TerrenceLynch <S01216934@acad.tri-c.edu>
2020-09-10 19:03:34 +0000

    Fixed some spelling mistakes.

--- main.cs
+++ main.cs
@@ -6,10 +6,10 @@ class MainClass {
     string item = Console.ReadLine();
     Console.WriteLine("Enter the quanity of " + item + "s");
     double quanity = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Enter the price for a" + item);
+    Console.WriteLine("Enter the price for a " + item);
     double price = Convert.ToDouble(Console.ReadLine());
     double total_price = (quanity * price);
-    Console.WriteLine("The total price of the" + item + "s is " + total_price);
+    Console.WriteLine("The total price of the " + item + "s is " + total_price);
 
   }
 }
